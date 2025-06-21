f39136940b578f278138a06ace676eb6edf0ddb7
DeepanS2 <sayanthandeepan@gmail.com>
2025-06-20 23:34:53 -0700

    Add files via upload

---
+++ DatabaseManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UnicomTICManagement_Project.Repositories
+{
+   
+    
+        public static class DatabaseManager
+        {
+            private static string dbFile = "unicomtic.db";
+            private static string connectionString = $"Data Source={dbFile};Version=3;";
+
+            public static void InitializeDatabase()
+            {
+                if (!File.Exists(dbFile))
+                {
+                    SQLiteConnection.CreateFile(dbFile);
+                }
+
+                using (var connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    Data.CreateTables(connection);
+                }
+            }
+
+            // ✅ This is the missing method
+            public static SQLiteConnection GetConnection()
+            {
+                return new SQLiteConnection(connectionString);
+            }
+        }
+    
+}
+
