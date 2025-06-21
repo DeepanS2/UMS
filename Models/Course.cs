bf6c7811dd6d347ac086352e691a480624739891
DeepanS2 <sayanthandeepan@gmail.com>
2025-06-20 23:07:11 -0700

    Add files via upload

---
+++ Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnicomTICManagement_Project.Models
+{
+    internal class Course
+    {
+        public int Id { get; set; }
+        public string CourseName { get; set; }
+    }
+}
